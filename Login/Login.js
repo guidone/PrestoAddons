e69ce667b958a9206a2ee4c4117abddd36545d90
Guidone <guido@gild.com>
2014-01-05 12:51:44 +0100

    login() with deferred and popup

--- Login/Login.js
+++ Login/Login.js
@@ -13,12 +13,12 @@ TODO
 - controllo email valida
 - controllo password uguali
 - recupero password
-	
+
 */
 
 /**
 * @class presto.addons.Login
-* Simple HTML page, create a menu, fetch content from the cloud 
+* Simple HTML page, create a menu, fetch content from the cloud
 * @extend presto.Plugin
 */
 var Login = Plugin.extend({
@@ -45,7 +45,7 @@ var Login = Plugin.extend({
 		that.app.loader.show();
 		that.app.facebook.authorize()
 			.done(function(appToken) {
-				
+
 				that.app.session.loginWithFacebook(appToken)
 					.done(function(user) {
 						// refresh the window
@@ -62,37 +62,38 @@ var Login = Plugin.extend({
 			})
 			.fail(function() {
 				that.app.loader.hide();
-				
+
 				// login failed, perhaps the session on facebook elapsed, ask to deauthorize
 				dialogs.confirm(L('login_FacebookAuthorizationFailedDeauthorize'))
 					.done(function() {
 						that.app.facebook.deauthorize();
 					});
-				
+
 			});
-		
+
 		return that;
 	},
 
 	/**
 	* @method getEvents
 	* Add an on click event to the list
+	* @protected
 	*/
 	getEvents: function() {
-		
+
 		var that = this;
 		var events = that._super.call(that,arguments);
-		
+
 		events = _.extend(events,{
 			'.pr-button-login-facebook': {
 				'click': function() {
 					that.doLoginWithFacebook();
-				}			
+				}
 			},
 			'.pr-login-twitter': {
 				'click': function() {
 					Ti.API.info('twitter');
-				}			
+				}
 			},
 			'.pr-login-linkedin': {
 				'click': function() {
@@ -101,18 +102,26 @@ var Login = Plugin.extend({
 			},
 			'.pr-button-login': {
 				'click': function(evt) {
-					that.doLogin(evt);	
-				}	
+
+					// check valid form
+					var check = that.validateLoginForm();
+					if (check != null) {
+						alert(check);
+						return false;
+					}
+
+					that.executeLogin(evt);
+				}
 			},
 			'.pr-button-logout': {
 				'click': function(evt) {
-					that.doLogout(evt);	
-				}	
+					that.doLogout(evt);
+				}
 			},
-			
+
 			'#btnSwitchPushMessages': {
 				'change': function(evt) {
-					
+
 					if (evt.value) {
 						that.app.push.subscribe()
 							.done(function() {
@@ -121,7 +130,7 @@ var Login = Plugin.extend({
 							.fail(function(e) {
 								alert(L('login_ErrorTryingToSubscribe'));
 								that.getLayoutManager().getById('btnSwitchPushMessages').value = false;
-							});						
+							});
 					} else {
 						that.app.push.unsubscribe()
 							.done(function() {
@@ -130,69 +139,63 @@ var Login = Plugin.extend({
 							.fail(function(e) {
 								alert(L('login_ErrorTryingToUnsubscribe'));
 								that.getLayoutManager().getById('btnSwitchPushMessages').value = true;
-							});						
+							});
 					}
-							
+
 				}
-			}						
+			}
 		});
-		
+
 		return events;
 	},
-	
+
 	/**
 	* @method getVariables
 	* Get variables for the login form
 	* @return {Object}
+	* @protected
 	*/
 	getVariables: function() {
-		
+
 		var that = this;
 		var result = that._super.apply(that,arguments);
-		
+
 		var user = that.app.config.get('session','user');
-		
+
 		result = _.extend(result,{
 			isLogged: that.app.session.hasSessionOrToken(),
 			isSubscribed: that.app.push != null && that.app.push.isSubscribed(),
 			user: user,
-			isFacebook: that.app.facebook != null 
+			isFacebook: that.app.facebook != null
 		});
-			
+
 		return result;
 	},
-	
+
 	/**
 	* @method getDefaults
 	* Get default options of the plugin
 	* @return {Object}
 	*/
 	getDefaults: function() {
-	
+
 		var that = this;
 		var result = that._super();
-		
+
 		result = _.extend(result,{
 
-			/**
-			* @cfg {Function} onClick
-			* Executed on click of element, second argument is the clicked Backbone model
-			*/
-			onClick: null		
-			
+			// fix the id
+			id: 'login'
+
 		});
-		
-		//if (that.app.facebook != null) {
-		//	result.tags.FACEBOOKBUTTON = '/addons/Facebook/layout/button.js';			
-		//}
-		
+
 		return result;
 	},
 
 	getSignupAction: function() {
-		
+
 		var that = this;
-		
+
 		return {
 			label: L('Signup'),
 			execute: function() {
@@ -208,7 +211,7 @@ var Login = Plugin.extend({
 	* @return {presto.models.Action}
 	*/
 	getAction: function() {
-		
+
 		var that = this;
 
 		if (that.app.session.isLogged()) {
@@ -222,7 +225,7 @@ var Login = Plugin.extend({
 	doLogout: function() {
 
 		var that = this;
-		
+
 		that.app.session.logout()
 			.done(function(result) {
 
@@ -232,39 +235,32 @@ var Login = Plugin.extend({
 				that.app.fireEvent('auth_logout');
 				// put signup action
 				that.setAction(that.getSignupAction());
-				
+
 			}).fail(function(err) {
 				Ti.API.error('Error on logging out');
 			});
-		
+
 	},
-	
+
 	/**
-	* @method doLogin
+	* @method executeLogin
 	* Executes login, check form, fire event and swap button
 	* @private
+	* @deferred
 	*/
-	doLogin: function() {
+	executeLogin: function() {
 
 		var that = this;
+		var deferred = jQ.Deferred();
 		var layoutManager = that.getLayoutManager();
 		var username = layoutManager.getById('username').getValue();
 		var password = layoutManager.getById('password').getValue();
-
-		if (_.isEmpty(username)) {
-			alert(L('InsertEmailToLogin'));
-			return false;
-		}
-		if (_.isEmpty(password)) {
-			alert(L('InsertPasswordToLogin'));
-			return false;
-		}
 
 		that.app.session.login(
 			username,
 			password
 		).done(function(result) {
-			
+
 			//Ti.API.info('logged'+JSON.stringify(result));
 			// refresh the window
 			that.refresh();
@@ -272,11 +268,64 @@ var Login = Plugin.extend({
 			that.app.fireEvent('auth_login');
 			// remove signup action
 			that.setAction(null);
-			
+			// resolve
+			deferred.resolve();
+
 		}).fail(function(e) {
+
 			alert(L('login_ErrorOnLogin')+': '+e.message);
+			// reject
+			deferred.reject();
+
 		});
-		
+
+		return deferred.promise();
+	},
+
+	doLogin: function() {
+
+		var that = this;
+		var deferred = jQ.Deferred();
+		var layoutManager = that.getLayoutManager();
+		var username = layoutManager.getById('username').getValue();
+		var password = layoutManager.getById('password').getValue();
+
+		that.app.session.login(
+			username,
+			password
+		).done(function(result) {
+
+			// fire login event
+			that.app.fireEvent('auth_login');
+			// resolve
+			deferred.resolve();
+
+		}).fail(function(e) {
+
+			alert(L('login_ErrorOnLogin')+': '+e.message);
+			// reject
+			deferred.reject();
+
+		});
+
+		return deferred.promise();
+	},
+
+	validateLoginForm: function() {
+
+		var that = this;
+		var layoutManager = that.getLayoutManager();
+		var username = layoutManager.getById('username').getValue();
+		var password = layoutManager.getById('password').getValue();
+
+		if (_.isEmpty(username)) {
+			return L('InsertEmailToLogin');
+		}
+		if (_.isEmpty(password)) {
+			return L('InsertPasswordToLogin');
+		}
+
+		return null;
 	},
 
 	/**
@@ -300,7 +349,7 @@ var Login = Plugin.extend({
 		}
 		if (_.isEmpty(signup_last_name)) {
 			return L('login_InsertYourLastNameToSignup');
-		}		
+		}
 		if (_.isEmpty(username)) {
 			return L('login_InsertValidEmailToSignup');
 		}
@@ -310,7 +359,7 @@ var Login = Plugin.extend({
 		if (password != password_confirmation) {
 			return L('login_PasswordsDontMatch');
 		}
-		
+
 		return null;
 	},
 
@@ -321,9 +370,9 @@ var Login = Plugin.extend({
 	* @deferred
 	*/
 	doSignup: function() {
-		
+
 		// guido.bellomo+2@gmail.com
-		
+
 		var that = this;
 		var deferred = jQ.Deferred();
 		var layoutManager = that.getLayoutManager();
@@ -332,10 +381,10 @@ var Login = Plugin.extend({
 		var password_confirmation = layoutManager.getById('signup_password_confirmation').getValue();
 		var signup_first_name = layoutManager.getById('signup_last_name').getValue();
 		var signup_last_name = layoutManager.getById('signup_last_name').getValue();
-				
+
 		jQ.when(that.app.privacy != null ? that.app.privacy.show() : true)
 			.done(function() {
-			
+
 				Cloud.Users.create({
 					first_name: signup_first_name,
 					last_name: signup_last_name,
@@ -343,43 +392,40 @@ var Login = Plugin.extend({
 					password: password,
 					password_confirmation: password_confirmation
 				},function(e) {
-					
+
 					if (e.success) {
-						Ti.API.info('signup '+JSON.stringify(e));
+
 						// refresh the window
 						//that.refresh();
-						// fire login event				
+						// fire login event
 						// include the password
 						e.users[0].password = password;
 						// now perform login
 						deferred.resolve(e);
-										
+
 					} else {
-						// hide the loader				
-						deferred.reject(e);	
+						// hide the loader
+						deferred.reject(e);
 					}
 				});
-				
-			});	
 
-		
+			});
+
+
 		return deferred.promise();
 	},
-	
+
 	/**
 	* @method showSubscribe
 	* Show a photo in a modal overlay
-	* @param {presto.models.Photo} photo
 	*/
 	showSubscribe: function() {
-		
+
 		Ti.API.info('showSubscribe@login');
+
 		var that = this;
 		var layoutManager = that.getLayoutManager();
 		var navigation = that.getNavigation();
-		//var options = that.getOptions();
-		
-		// set the photo to be displayed
 		var layout = that.getWindowLayout();
 
 		layout.childs = [
@@ -397,8 +443,8 @@ var Login = Plugin.extend({
 
 		// close events
 		var close = function(evt) {
-			navigation.closeWindow(signupWindow);	
-		};		
+			navigation.closeWindow(signupWindow);
+		};
 		layoutManager.events({
 			'.pr-toolbarbtn-back': {
 				'click': close
@@ -417,7 +463,7 @@ var Login = Plugin.extend({
 						alert(check);
 						return false;
 					}
-					
+
 					// the start saving
 					that.app.loader.show(L('login_SigningUp'));
 					that.doSignup()
@@ -426,7 +472,7 @@ var Login = Plugin.extend({
 								msg.users[0].email,
 								msg.users[0].password
 							).done(function(result) {
-								
+
 								// close the window
 								navigation.closeWindow(signupWindow);
 								// refresh the window
@@ -435,13 +481,13 @@ var Login = Plugin.extend({
 								that.app.fireEvent('auth_signup');
 								// remove signup action
 								that.setAction(null);
-			
+
 							}).fail(function(e) {
 								alert(L('login_ErrorOnLogin')+': '+e.message);
 							}).always(function() {
 								that.app.loader.hide();
 							});
-								
+
 						})
 						.fail(function(e) {
 							that.app.loader.hide();
@@ -451,9 +497,128 @@ var Login = Plugin.extend({
 				}
 			}
 		});
-		
+
 		navigation.openWindow(signupWindow);
-						
+
+	},
+
+
+	/**
+	* @method login
+	* Executes the login in a popup window, it's possibile to signup. If the user is already logged, it resolves immediately
+	* @deferred
+	*/
+	login: function() {
+
+		Ti.API.info('showSubscribe@login');
+
+		var deferred = jQ.Deferred();
+		var that = this;
+		var app = that.app;
+
+		// resolve immediately if already logged
+		if (app.session.isLogged()) {
+			Ti.API.info('Already logged, skipping');
+			deferred.resolve();
+			return deferred.promise();
+		}
+
+		// ok start the login
+		var layoutManager = that.getLayoutManager();
+		var navigation = that.getNavigation();
+		var layout = that.getWindowLayout({
+			leftButton: {
+				type: 'button',
+				className: 'pr-toolbarbtn pr-toolbarbtn-back'
+			}
+		});
+
+		layout.childs = [
+			{
+				type: 'tag',
+				name: 'LOGINFORM'
+			}
+		];
+
+		// create the layout
+		var loginWindow = layoutManager.createLayout(layout);
+		var loginNavigator = Ti.UI.iOS.createNavigationWindow({
+			window: loginWindow,
+			top: Ti.Platform.displayCaps.platformWidth
+		});
+		var close = function() {
+			var slideDown = Titanium.UI.createAnimation();
+			slideUp.top = Ti.Platform.displayCaps.platformWidth;
+			slideUp.duration = 200;
+			slideUp.curve = Ti.UI.ANIMATION_CURVE_EASE_OUT;
+			loginNavigator.animate(slideUp,function() {
+				loginNavigator.close();
+				deferred.reject();
+			});
+		};
+
+		// close events
+		layoutManager.events({
+			'.pr-button-login-facebook': {
+				'click': function() {
+					that.doLoginWithFacebook();
+				}
+			},
+			'.pr-login-twitter': {
+				'click': function() {
+					Ti.API.info('twitter');
+				}
+			},
+			'.pr-login-linkedin': {
+				'click': function() {
+					Ti.API.info('linkedin');
+				}
+			},
+			'.pr-button-login': {
+				'click': function(evt) {
+
+					// check valid form
+					var check = that.validateLoginForm();
+					if (check != null) {
+						alert(check);
+						return false;
+					}
+
+					that.app.loader.show(L('login_LoggingIn'));
+					that.doLogin(evt)
+						.done(function() {
+							close();
+							deferred.resolve();
+						})
+						.fail(function() {
+							deferred.reject();
+						})
+						.always(function() {
+							that.app.loader.hide();
+						});
+				}
+			},
+
+			'.pr-toolbarbtn-back': {
+				'click': function() {
+					close();
+				}
+			}
+		});
+
+		loginNavigator.open({
+			fullscreen: true,
+			modal: false,
+			animated: true,
+			modalTransitionStyle: Titanium.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL
+		});
+		var slideUp = Titanium.UI.createAnimation();
+		slideUp.top = 0;
+		slideUp.duration = 200;
+		slideUp.curve = Ti.UI.ANIMATION_CURVE_EASE_IN;
+		loginNavigator.animate(slideUp);
+
+		return deferred.promise();
 	}
 
 
